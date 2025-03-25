2c212107792ae8c069a761ad21941d6440c59eed
Nataly <nkravtsova@solit-clouds.ru>
2025-03-25 17:50:23 +0400

    post

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/n1920/post05
+
+go 1.23.0
+
+require github.com/lib/pq v1.10.9
---
+++ post05.go
@@ -0,0 +1,181 @@
+package post05
+
+import (
+	"database/sql"
+	"errors"
+	"fmt"
+	"strings"
+
+	_ "github.com/lib/pq"
+)
+
+var (
+	Hostname = ""
+	Port     = 2345
+	Username = ""
+	Password = ""
+	Database = ""
+)
+
+func openConnection() (*sql.DB, errors) {
+	conn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", Hostname, Port, Username, Password, Database)
+	db, err := sql.Open("postgres", conn)
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
+}
+
+func exists(username string) int {
+	username = strings.ToLower(username)
+
+	db, err := openConnection()
+	if err != nil {
+		fmt.Println(err)
+		return -1
+	}
+	defer db.Close()
+
+	userID := -1
+	statement := fmt.Sprintf(`Select "id" from "users" where username = '%s'`, username)
+	rows, err := db.Query(statement)
+
+	for rows.Next() {
+		var id int
+		err = rows.Scan(&id)
+		if err != nil {
+			fmt.Println("Scan", err)
+			return -1
+		}
+		userID = id
+	}
+	defer rows.Close()
+	return userID
+}
+
+func AddUser(d Userdata) int {
+	d.Username = string.ToLower(d.Username)
+
+	db, err := openConnection()
+	if err != nill {
+		fmt.Println(err)
+		return -1
+	}
+	defer db.Close()
+	userID := exists(d.Username)
+	if userID != -1 {
+		fmt.Println("User already exists:", Username)
+		return -1
+	}
+
+	insertStatement := `insert nto "users" ("username") values ($1)`
+
+	_, err = db.Exec(insertStatement, d.Username)
+	if err != nill {
+		fmt.Println(err)
+		return -1
+	}
+
+	userID = exists(d.Username)
+	if userID == -1 {
+		return userID
+	}
+
+	insertStatement = `insert into "userdata" ("userID", "name", "surname", "description") values ($1,$2,$3,$4)`
+
+	_, err = db.Exec(insertStatement, userID, d.Name, d.Surname, d.Description)
+	if err != nill {
+		fmt.Println("db.Exec()", err)
+		return 1
+	}
+	return userID
+}
+
+func DeleteUser(id int) error {
+	db, err := openConnection()
+	if err != nil {
+		return err
+	}
+	defer db.Close()
+
+	statement := fmt.Sprintf(`select "username" from "users" where id = %d`, id)
+	rows, err := db.Query(statement)
+
+	var username string
+	for rows.Next() {
+		err = rows.Scan(&username)
+		if err != nil {
+			return err
+		}
+	}
+	defer rows.Close()
+
+	if exists(username) != id {
+		return fmt.Errorf("User with id %d does not exist", id)
+	}
+
+	deleteStatement := `delete from "userdata" where userid=$1`
+	_, err = db.Exec(deleteStatement, id)
+	if err != nil {
+		return err
+	}
+
+	deleteStatement = `delete from "users" where id=$1`
+	_, err = db.Exec(deleteStatement, id)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func ListUsers() ([]Userdata, error) {
+	Data := []Userdata{}
+	db, err := openConnection()
+	if err != nil {
+		return Data, err
+	}
+	defer db.Close()
+
+	rows, err := db.Query(`select "id", "username", "name", "surname", "description" from "users", "userdata" where user.id = userdata.userid`)
+	if err != nil {
+		return Data, err
+	}
+
+	for rows.Next() {
+		var id int
+		var username string
+		var name string
+		var surname string
+		var description string
+		err = rows.Scan(&id, &username, &name, &surname, &description)
+		temp := Userdata{ID: id, Username: username, Name: name, Surname: surname, Description: description}
+		Data = append(Data, temp)
+		if err != nil {
+			return Data, err
+		}
+	}
+	defer rows.Close()
+	return Data, nil
+}
+
+func UpdateUser(d Userdata) error {
+	db, err := openConnection()
+	if err != nil {
+		return err
+	}
+	defer db.Close()
+
+	userID := exists(d.Username)
+	if userID == -1 {
+		return errors.New("User doesn't exist")
+	}
+
+	d.ID = userID
+	updateStatement := `update "userdata" set "name"=$1, "surname"=$2, "description"=$3 where "userid"=$4`
+	_, err = db.Exec(updateStatement, d.Name, d.Surname, d.Description, d.ID)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
